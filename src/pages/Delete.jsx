7a9e3b9f95e044738f4141c3a4b7cadb98b1e0e4
Felipe <sr.felipe.viana@gmail.com>
2022-04-07 20:01:22 -0300

    atualizacao

--- src/pages/Delete.jsx
+++ src/pages/Delete.jsx
@@ -5,7 +5,7 @@ export default function Delete(){
     return(
         <div className='App'>
             <h1>Delete um Usuario</h1>
-            <Link className='link' to="/">Home</Link>
+            <Link className='link' to="/">Pagina inicial</Link>
             <main className='formbox'>
                 <form action="https://gitmongo.herokuapp.com/delete" method='post' className="form">
                     <input type="name" name="name" placeholder='Nome' />
