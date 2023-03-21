14e54fd4ee5a95b3687c6390c04cbe2e3d7307fd
FelipeViana <56984834+FelipeDevJs@users.noreply.github.com>
2023-03-20 23:18:31 -0300

    update cors config

--- src/Components/GetUsers.jsx
+++ src/Components/GetUsers.jsx
@@ -7,7 +7,7 @@ export default function GetUser(){
     const [users, setUsers] = useState([])
 
     useEffect(()=>{
-        fetch('https://crudmongo-2kgo.onrender.com/all')
+        fetch('https://crudmongo-2kgo.onrender.com/all', { mode: 'no-cors' })
             .then((res)=> res.json())
             .then(data => setUsers(data))
             // console.log(users)
