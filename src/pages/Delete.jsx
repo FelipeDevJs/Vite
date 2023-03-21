f19f746ad911a6c783219868d295a55d45d2af56
FelipeViana <56984834+FelipeDevJs@users.noreply.github.com>
2023-03-20 23:37:11 -0300

    Update Delete.jsx

--- src/pages/Delete.jsx
+++ src/pages/Delete.jsx
@@ -8,7 +8,7 @@ export default function Delete(){
             <Link className='link' to="/">Home</Link>
             <main className='formbox'>
                 <form action="https://crudmongo-2kgo.onrender.com/delete" method='get' className="form">
-                    <input type="name" name="name" placeholder='Nome' />
+                    <input type="name" name="id" placeholder='ID User' />
                     <button type='submit'>Deletar</button>
                 </form>
             </main>
